702439fdc438e2be72b6a9d262ac0902d02d0461
Daniel-V-Richardson <richardson20june@gmail.com>
2022-12-26 07:48:57 +0530

    Bug Fixed

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
-import express from 'express'
-import * as dotenv from 'dotenv'
-import cors from 'cors'
-import { Configuration, OpenAIApi } from 'openai'
+import express from "express";
+import * as dotenv from "dotenv";
+import cors from "cors";
+import { Configuration, OpenAIApi } from "openai";
 
-dotenv.config()
+dotenv.config();
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
@@ -11,38 +11,40 @@ const configuration = new Configuration({
 
 const openai = new OpenAIApi(configuration);
 
-const app = express()
-app.use(cors())
-app.use(express.json())
+const app = express();
+app.use(cors());
+app.use(express.json());
 
-app.get('/', async (req, res) => {
+app.get("/", async (req, res) => {
   res.status(200).send({
-    message: 'Hello from Mark...'
-  })
-})
+    message: "Hello from CodeX!",
+  });
+});
 
-app.post('/', async (req, res) => {
+app.post("/", async (req, res) => {
   try {
     const prompt = req.body.prompt;
 
     const response = await openai.createCompletion({
-      model: "text-davinci-003",
-      prompt: `${prompt}`,
-      temperature: 0.7,
-      max_tokens: 500,
+      model: "code-davinci-003",
+      prompt: "Add Two Numbers in Java",
+      // prompt: `${prompt}`,
+      temperature: 0,
+      max_tokens: 3000,
       top_p: 1,
-      frequency_penalty: 0,
+      frequency_penalty: 0.5,
       presence_penalty: 0,
     });
 
     res.status(200).send({
-      bot: response.data.choices[0].text
+      bot: response.data.choices[0].text,
     });
-
   } catch (error) {
-    console.error(error)
-    res.status(500).send(error || 'Something went wrong');
+    console.error(error);
+    res.status(500).send(error || "Something went wrong");
   }
-})
+});
 
-app.listen(5000, () => console.log('AI server started on http://localhost:5000'))
+app.listen(5000, () =>
+  console.log("AI server started on http://localhost:5000")
+);
