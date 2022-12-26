3748e41af20145c70962935789dff05ce4e391cc
Daniel-V-Richardson <richardson20june@gmail.com>
2022-12-26 07:48:28 +0530

    Bug Fixed

--- client/script.js
+++ client/script.js
@@ -1,121 +1,109 @@
-import bot from './assets/bot.svg'
-import user from './assets/user.svg'
+import bot from "./assets/bot.svg";
+import user from "./assets/user.svg";
 
-const form = document.querySelector('form')
-const chatContainer = document.querySelector('#chat_container')
+const form = document.querySelector("form");
+const chatContainer = document.querySelector("#chat_container");
 
-let loadInterval
+let loadInterval;
 
 function loader(element) {
-    element.textContent = ''
+  element.textContent = "";
 
-    loadInterval = setInterval(() => {
-        // Update the text content of the loading indicator
-        element.textContent += '.';
+  loadInterval = setInterval(() => {
+    element.textContent += ".";
 
-        // If the loading indicator has reached three dots, reset it
-        if (element.textContent === '....') {
-            element.textContent = '';
-        }
-    }, 300);
+    if (element.textContent === "....") {
+      element.textContent = "";
+    }
+  }, 300);
 }
 
 function typeText(element, text) {
-    let index = 0
+  let index = 0;
 
-    let interval = setInterval(() => {
-        if (index < text.length) {
-            element.innerHTML += text.charAt(index)
-            index++
-        } else {
-            clearInterval(interval)
-        }
-    }, 20)
+  let interval = setInterval(() => {
+    if (index < text.length) {
+      element.innerHTML += text.charAt(index);
+      index++;
+    } else {
+      clearInterval(interval);
+    }
+  }, 20);
 }
 
-// generate unique ID for each message div of bot
-// necessary for typing text effect for that specific reply
-// without unique ID, typing text will work on every element
 function generateUniqueId() {
-    const timestamp = Date.now();
-    const randomNumber = Math.random();
-    const hexadecimalString = randomNumber.toString(16);
+  const timestamp = Date.now();
+  const randomNumber = Math.random();
+  const hexadecimalString = randomNumber.toString(16);
 
-    return `id-${timestamp}-${hexadecimalString}`;
+  return `id-${timestamp}-${hexadecimalString}`;
 }
 
 function chatStripe(isAi, value, uniqueId) {
-    return (
-        `
-        <div class="wrapper ${isAi && 'ai'}">
+  return `
+        <div class="wrapper ${isAi && "ai"}">
             <div class="chat">
                 <div class="profile">
                     <img 
                       src=${isAi ? bot : user} 
-                      alt="${isAi ? 'bot' : 'user'}" 
+                      alt="${isAi ? "bot" : "user"}" 
                     />
                 </div>
-                <div class="message ${isAi && 'user'}" id=${uniqueId}>${value}</div>
+                <div class="message ${
+                  isAi && "user"
+                }" id=${uniqueId}>${value}</div>
             </div>
         </div>
-    `
-    )
+    `;
 }
-
 
 const handleSubmit = async (e) => {
-    e.preventDefault()
+  e.preventDefault();
 
-    const data = new FormData(form)
+  const data = new FormData(form);
 
-    // user's chatstripe
-    chatContainer.innerHTML += chatStripe(false, data.get('prompt'))
+  chatContainer.innerHTML += chatStripe(false, data.get("prompt"));
 
-    // to clear the textarea input 
-    form.reset()
+  form.reset();
 
-    // bot's chatstripe
-    const uniqueId = generateUniqueId()
-    chatContainer.innerHTML += chatStripe(true, " ", uniqueId)
+  const uniqueId = generateUniqueId();
+  chatContainer.innerHTML += chatStripe(true, " ", uniqueId);
 
-    // to focus scroll to the bottom 
-    chatContainer.scrollTop = chatContainer.scrollHeight;
+  chatContainer.scrollTop = chatContainer.scrollHeight;
 
-    // specific message div 
-    const messageDiv = document.getElementById(uniqueId)
+  const messageDiv = document.getElementById(uniqueId);
 
-    // messageDiv.innerHTML = "..."
-    loader(messageDiv)
-//https://mark-sm64.onrender.com
-    const response = await fetch('https://mark-sm64.onrender.com/', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            prompt: data.get('prompt')
-        })
-    })
+  loader(messageDiv);
+  //https://mark-sm64.onrender.com
+  const response = await fetch("https://mark-sm64.onrender.com/", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      prompt: data.get("prompt"),
+    }),
+  });
 
-    clearInterval(loadInterval)
-    messageDiv.innerHTML = " "
+  clearInterval(loadInterval);
+  messageDiv.innerHTML = " ";
 
-    if (response.ok) {
-        const data = await response.json();
-        const parsedData = data.bot.trim() // trims any trailing spaces/'\n' 
+  if (response.ok) {
+    const data = await response.json();
+    const parsedData = data.bot.trim();
 
-        typeText(messageDiv, parsedData)
-    } else {
-        const err = await response.text()
+    typeText(messageDiv, parsedData);
+  } else {
+    const err = await response.text();
 
-        messageDiv.innerHTML = "Something went wrong"
-        alert(err)
-    }
-}
+    messageDiv.innerHTML = "Something went wrong";
+    alert(err);
+  }
+};
 
-form.addEventListener('submit', handleSubmit)
-form.addEventListener('keyup', (e) => {
-    if (e.keyCode === 13) {
-        handleSubmit(e)
-    }
-})
+form.addEventListener("submit", handleSubmit);
+form.addEventListener("keyup", (e) => {
+  if (e.keyCode === 13) {
+    handleSubmit(e);
+  }
+});
